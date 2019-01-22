125cd8d60c1396d52894b8a2c771d3b0223ba56d
Nick_Jiang <jiangshan@xianjincard.com>
2019-01-22 11:40:12 +0800

    alloc project  目前支持 babel 转义 jsx 文件， scss|less 文件， 支持启动开发环境 server，

---
+++ app/component.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Component.scss";
+
+export default class Hello extends React.Component {
+  render() {
+    return <h1 class="good">Hello world</h1>;
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+var path = require("path");
+// var node_modules = path.resolve(__dirname, "node_modules");
+// var pathToReact = path.resolve(node_modules, "react/dist/react.min.js");
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // 该插件的主要是为了抽离css样式,防止将样式打包在js中引起页面样式加载错乱的现象
+const devMode = process.env.NODE_ENV !== "production";
+
+var config = {
+  entry: ["webpack/hot/dev-server", path.resolve(__dirname, "app/main.js")],
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js"
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: devMode ? "[name].css" : "[name].[hash].css",
+      chunkFilename: devMode ? "[id].css" : "[id].[hash].css"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/, // 用正则来匹配文件路径，这段意思是匹配 js 或者 jsx
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            plugins: ["@babel/plugin-transform-react-jsx"]
+          }
+        }
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ]
+      }
+      //   {
+      //     test: /\.(jpg|png|gif|mp3|svg)$/,
+      //     loaders: ["file-loader"]
+      //   }
+    ]
+  }
+};
+
+module.exports = config;
