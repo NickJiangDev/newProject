971d871dec9a3d60a93ad300bb53b774e45dfc77
Nick_Jiang <jiangshan@xianjincard.com>
2019-02-19 14:14:20 +0800

    add react-router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import Hello from "./component.jsx";
 
 function main() {
-  ReactDOM.render(<Hello />, document.getElementById("app"));
+  ReactDOM.render(
+    <BrowserRouter>
+      <Route path="/home" component={Hello} />
+    </BrowserRouter>,
+    document.getElementById("app")
+  );
 }
 
 main();
